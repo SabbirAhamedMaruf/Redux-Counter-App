98e0164b03703c56de4046b3679821cbd8b6fdbb
SabbirAhamedMaruf <sabbirahamedmaruf02@gmail.com>
2024-01-16 10:16:42 +0600

    added incrment by value function

--- Redux-Counter-APP/src/pages/Home.jsx
+++ Redux-Counter-APP/src/pages/Home.jsx
@@ -1,13 +1,43 @@
 import { useSelector, useDispatch } from "react-redux";
-import { increment,decrement } from "../redux/features/counter/counterSlice";
+import {
+  increment,
+  decrement,
+  incrementByValue,
+} from "../redux/features/counter/counterSlice";
+import { useState } from "react";
 const Home = () => {
+  const [value, setValue] = useState(null);
   const count = useSelector((state) => state);
   const dispatch = useDispatch();
   return (
     <>
       <h1>Result is = {count.counter.count}</h1>
-      <button className="mr-10 bg-green-200 p-2" onClick={() => dispatch(increment())}>Increment</button>
-      <button className="mr-10 bg-red-200 p-2" onClick={() => dispatch(decrement())}>Decrement</button>
+      <button
+        className="mr-10 bg-green-200 p-2"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>{" "}
+      <br />
+      <button
+        className="mr-10 bg-red-200 p-2"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>{" "}
+      <br />
+      <input
+        type="text"
+        placeholder="Enter your number"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button
+        className="mr-10 bg-green-200 p-2"
+        onClick={() => dispatch(incrementByValue(parseInt(value)))}
+      >
+        Increment
+      </button>{" "}
+      <br />
     </>
   );
 };
